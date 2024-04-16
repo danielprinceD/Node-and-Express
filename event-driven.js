697d4eb45e7caf89a9a316f9e83d1eb7b48c7cec
daniel <d.danielprince2003@gmail.com>
2024-04-16 19:50:15 +0530

    feat:event driven

---
+++ event-driven.js
@@ -0,0 +1,23 @@
+//  const util = require('util')
+//  util.promisify(data)
+var EventEmitter = require("events");
+const { createServer } = require("http");
+
+class Class {
+  obj = {
+    name: "daniel",
+  };
+}
+const myClass = new Class();
+var myEmitter = new EventEmitter();
+myEmitter.on("response", () => {
+  console.log("Hi " + myClass.obj.name);
+});
+
+createServer((req, res) => {
+  if (req.url === "/") res.end("Hello");
+}).listen(5000, () => {
+  console.log("Running Server");
+});
+
+myEmitter.emit("response");
